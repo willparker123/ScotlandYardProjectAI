c99e54ef18b86825ae55d5dbf2d95bbf84aa53fa
Will <williamparker100@gmail.com>
2019-04-29 13:08:50 +0100

    ai alpha

--- ScotlandsYard/cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/SamiAI.java
+++ ScotlandsYard/cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/SamiAI.java
@@ -7,24 +7,14 @@ import java.util.function.Consumer;
 
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
-import uk.ac.bris.cs.scotlandyard.model.Colour;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.DoubleMove;
-import uk.ac.bris.cs.scotlandyard.model.PassMove;
-import uk.ac.bris.cs.scotlandyard.model.TicketMove;
-import uk.ac.bris.cs.scotlandyard.model.Player;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardModel;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Optional;
-import java.util.Set;
+import uk.ac.bris.cs.scotlandyard.model.*;
+import java.util.*;
 import static uk.ac.bris.cs.scotlandyard.model.Colour.*;
 import static uk.ac.bris.cs.scotlandyard.model.Ticket.*;
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
+import uk.ac.bris.cs.gamekit.graph.Node;
 
 @ManagedAI("SAMI")
 public class SamiAI implements PlayerFactory {
@@ -42,6 +32,7 @@ public class SamiAI implements PlayerFactory {
 		//executes different move decider logic depending on if this is true or false
 		private final boolean mrXAI;
 
+		private final Random random = new Random();
 		private MyPlayer(boolean isMrX) {
 			mrXAI = isMrX;
 		}
@@ -49,8 +40,9 @@ public class SamiAI implements PlayerFactory {
 		@Override
 		public void makeMove(ScotlandYardView view, int location, Set<Move> moves,
 				Consumer<Move> callback) {
-			ArrayList<int> moveScores = scoreMoves(view, location, moves);
-			ArrayList<int> moveScoresSorted = moveScores.sort();
+			ArrayList<Integer> moveScores = scoreMoves(view, location, moves);
+			ArrayList<Integer> moveScoresSorted = moveScores;
+			Collections.sort(moveScoresSorted);
 			int moveIndex;
 
 			if (mrXAI) {
@@ -64,8 +56,8 @@ public class SamiAI implements PlayerFactory {
 		}
 
 		//scores moves (high = best move, low = worst move)
-		private ArrayList<int> scoreMoves(ScotlandYardView view, int location, Set<Move> moves) {
-			ArrayList<int> scores = new ArrayList<>();
+		private ArrayList<Integer> scoreMoves(ScotlandYardView view, int location, Set<Move> moves) {
+			ArrayList<Integer> scores = new ArrayList<>();
 			//for each of the moves, score them and add the score to 'scores'
 			Iterator<Move> iterator = moves.iterator();
 			while (iterator.hasNext()) {
@@ -74,21 +66,28 @@ public class SamiAI implements PlayerFactory {
 			return scores;
 		}
 
-		private ArrayList<int> sortScores(ArrayList<int> scores) {
-			return scores.sort();
+		//TODO
+		private ArrayList<Integer> sortScores(ArrayList<Integer> scores) {
+			return scores;
 		}
 
+		private Integer score(Move m, ScotlandYardView view) {
+			if (m instanceof TicketMove) return score((TicketMove) m, view);
+			else if (m instanceof PassMove) return score((PassMove) m, view);
+			else if (m instanceof DoubleMove) return score((DoubleMove) m, view);
+			else return 0;
+		}
 		//good score: far from detectives, many validMoves with target node
-		private int score(TicketMove m, ScotlandYardView view) {
+		private Integer score(TicketMove m, ScotlandYardView view) {
 			int totalDistance = 0;
 			int totalValidMoves = 0;
 			//for all players, find the distance to mrX from the player
 			if (mrXAI) {
 				List<Colour> cs = view.getPlayers();
-				Iterator<Move> iterator = cs.iterator();
+				ListIterator<Colour> iterator = cs.listIterator();
 				while (iterator.hasNext()) {
 					//skip players with no location
-					if (view.getPlayerLocation(iterator.next())==Optional.empty()) continue;
+					if (!view.getPlayerLocation(iterator.next()).isPresent()) continue;
 					else {
 						//finds critical path to mrX from the player
 						totalDistance+=criticalPath(m.destination(), view.getPlayerLocation(iterator.next()).get());
@@ -96,37 +95,49 @@ public class SamiAI implements PlayerFactory {
 				}
 			} else {
 				List<Colour> cs = view.getPlayers();
-				//skip players with no location
-				if (view.getPlayerLocation(iterator.next())==Optional.empty()) continue;
-				else {
-					//finds critical path to mrX from the move's destination
-					totalDistance+=criticalPath(m.destination(), view.getPlayerLocation(cs.get(0)).get());
+				ListIterator<Colour> iterator = cs.listIterator();
+				while (iterator.hasNext()) {
+					//skip players with no location
+					if (!view.getPlayerLocation(iterator.next()).isPresent()) continue;
+					else {
+						//finds critical path to mrX from the move's destination
+						totalDistance+=criticalPath(m.destination(), view.getPlayerLocation(cs.get(0)).get());
+					}
 				}
 			}
 
 			try {
 				if (mrXAI) {
-					totalValidMoves = getValidMoves(BLACK, m.destination()).size();
-				} else totalValidMoves = getValidMoves(RED, m.destination()).size();
+					totalValidMoves = getValidMoves(BLACK, m.destination(), view).size();
+				} else totalValidMoves = getValidMoves(RED, m.destination(), view).size();
 			} catch (Exception e) {
 				totalValidMoves = 1;
 			}
 
-			if (mrXAI) return totalDistance*totalValidMoves;
-			else return totalDistance/totalValidMoves;
+			if (mrXAI) return Integer.valueOf(totalDistance*totalValidMoves);
+			else return Integer.valueOf(totalDistance/totalValidMoves);
 		}
+		//TODO
 		private int score(DoubleMove m, ScotlandYardView view) {
+			return random.nextInt(10000);
 			//good score: far from detectives, many validMoves with target node
 		}
+		//TODO
 		private int score(PassMove m, ScotlandYardView view) {
+			return random.nextInt(10000);
 			//good score: far from detectives, many validMoves with target node
 		}
 
+		//TODO
 		private int criticalPath(int x, int y) {
 			return x-y;
 		}
 
-		public Set<Move> getValidMoves(Colour colour, Integer location) {
+		public int roundRemaining(ScotlandYardView view) {
+			return view.getRounds().size() - view.getCurrentRound();
+		}
+
+		public Set<Move> getValidMoves(Colour colour, Integer location, ScotlandYardView view) {
 			Set<Move> cplayerMoves = new HashSet<>();
 			Node<Integer> nodeL = view.getGraph().getNode(location);
 			Collection<Edge<Integer, Transport>> e;
@@ -142,40 +153,40 @@ public class SamiAI implements PlayerFactory {
 				Ticket ticket = Ticket.fromTransport(edge.data());
 				//if the reachable nodes don't have a player on them and the player has available tickets,
 				// add this node as a possible TicketMove
-				if (!destinationHasPlayer(destination)) {
-					if (playerHasTicketsAvailable(colour, ticket)) {
+				if (!destinationHasPlayer(view,destination)) {
+					if (playerHasTicketsAvailable(view,colour, ticket)) {
 						cplayerMoves.add(new TicketMove(colour, ticket, destination));
 					}
-					if (playerHasTicketsAvailable(colour, SECRET)) {
+					if (playerHasTicketsAvailable(view,colour, SECRET)) {
 						cplayerMoves.add(new TicketMove(colour, SECRET, destination));
 					}
-					if (playerHasTicketsAvailable(colour, DOUBLE) && roundRemaining() >= 2) {
-						Node<Integer> nodeR = graph.getNode(destination);
+					if (playerHasTicketsAvailable(view,colour, DOUBLE) && roundRemaining(view) >= 2) {
+						Node<Integer> nodeR = view.getGraph().getNode(destination);
 						Collection<Edge<Integer, Transport>> d;
 						if (nodeR == null) {
-							d = graph.getEdges();
+							d = view.getGraph().getEdges();
 						} else {
-							d = graph.getEdgesFrom(Objects.requireNonNull(nodeR));
+							d = view.getGraph().getEdgesFrom(Objects.requireNonNull(nodeR));
 						}
 						for (Edge<Integer, Transport> edge1 : d) {
 							Integer destination1 = edge1.destination().value();
 							Ticket ticket1 = Ticket.fromTransport(edge1.data());
-							boolean tickets = (ticket==ticket1 && playerHasTicketsAvailable(colour,ticket,2))
-									|| (ticket != ticket1 && playerHasTicketsAvailable(colour,ticket1));
+							boolean tickets = (ticket==ticket1 && playerHasTicketsAvailable(view,colour,ticket,2))
+									|| (ticket != ticket1 && playerHasTicketsAvailable(view,colour,ticket1));
 							TicketMove firstMove = new TicketMove(colour, ticket, destination);
 							TicketMove secondMove = new TicketMove(colour, ticket1, destination1);
 
-							if ((destination1 == location || !destinationHasPlayer(destination1)) && tickets) {
+							if ((destination1 == location || !destinationHasPlayer(view,destination1)) && tickets) {
 								cplayerMoves.add(new DoubleMove(colour, firstMove, secondMove));
-							} if ((destination1 == location || !destinationHasPlayer(destination1))
-									&& playerHasTicketsAvailable(colour,SECRET)) {
+							} if ((destination1 == location || !destinationHasPlayer(view,destination1))
+									&& playerHasTicketsAvailable(view,colour,SECRET)) {
 								TicketMove secretFirstMove = new TicketMove(colour,SECRET,destination);
 								TicketMove secretSecondMove = new TicketMove(colour,SECRET,destination1);
-								if (playerHasTicketsAvailable(colour,SECRET,2)) {
+								if (playerHasTicketsAvailable(view,colour,SECRET,2)) {
 									cplayerMoves.add(new DoubleMove(colour,secretFirstMove,secretSecondMove));
-								} if (playerHasTicketsAvailable(colour,ticket)) {
+								} if (playerHasTicketsAvailable(view,colour,ticket)) {
 									cplayerMoves.add(new DoubleMove(colour,firstMove,secretSecondMove));
-								} if (playerHasTicketsAvailable(colour,ticket1)) {
+								} if (playerHasTicketsAvailable(view,colour,ticket1)) {
 									cplayerMoves.add(new DoubleMove(colour,secretFirstMove,secondMove));
 								}
 							}
@@ -188,20 +199,29 @@ public class SamiAI implements PlayerFactory {
 			return cplayerMoves;
 		}
 
-		public boolean destinationHasPlayer(int i) {
-			for (ScotlandYardPlayer p : players) {
-				if (p.location()==i && p.colour().isDetective()) return true;
+		public boolean destinationHasPlayer(ScotlandYardView view, int i) {
+			for (Colour p : view.getPlayers()) {
+				if (view.getPlayerLocation(p).get()==i && p.isDetective()) return true;
 			}
 			return false;
 		}
 
-		public boolean playerHasTicketsAvailable(Colour colour, Ticket ticket) {
+		public boolean playerHasTicketsAvailable(ScotlandYardView view, Colour colour, Ticket ticket) {
 			int i;
 			try {
-				i = getPlayerTickets(colour, ticket).get();
+				i = view.getPlayerTickets(colour, ticket).get();
 			} catch (NoSuchElementException e) {
 				i = 0;
 			} if (i>=1) return true;
+			else return false;
+		}
+		public boolean playerHasTicketsAvailable(ScotlandYardView view, Colour colour, Ticket ticket, int n) {
+			int i;
+			try {
+				i = view.getPlayerTickets(colour, ticket).get();
+			} catch (NoSuchElementException e) {
+				i = 0;
+			} if (i>=n) return true;
 			else return false;
 		}
 	}
